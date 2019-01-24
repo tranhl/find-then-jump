c98c0e8c3372a73f51e5e2e81fec3231b185b05c
Long Hoang Tran <tranhl@gmx.com>
2019-01-24 23:57:20 +1100

    refactor: improved readability of iteration order algorithm

--- src/documentScanner.ts
+++ src/documentScanner.ts
@@ -7,6 +7,22 @@ type Match = {
   excludedChars: string[],
 }
 
+enum ScanDirection {
+  Up,
+  Down,
+}
+
+type ScannerState = {
+  scanDirection: ScanDirection,
+  initialCursorPosition: Readonly<number>,
+  upperCursorPosition: number,
+  lowerCursorPosition: number,
+  upperBoundary: Readonly<number>,
+  lowerBoundary: Readonly<number>,
+  upperBoundaryReached: boolean,
+  lowerBoundaryReached: boolean,
+}
+
 class DocumentScanner implements IterableIterator<any> {
   // When generating a list of valid jump characters, there are cases where
   // the user continues typing, expecting to narrow down their search, but
@@ -14,127 +30,80 @@ class DocumentScanner implements IterableIterator<any> {
   // search match, we can prevent this from happening. This value was chosen
   // through trial and error, and prevents the above case from happening.
   static EXCLUSION_LOOKAHEAD_LENGTH: number = 8
-  static MAX_LINES_TO_SCAN: number = 300
+  static ITERATION_LIMIT: number = 200 // Prevents degraded performance for large files
   static NON_ALPHABETS: RegExp = /[^a-z]/gi
 
   readonly document: TextDocument
-  scanOrder: number[]
+  scannerState: ScannerState
+  iterationOrder: number[]
   documentIterator: Iterator<any>
 
-  constructor(document: Readonly<TextDocument>, startLine: number, needle: string) {
+  constructor(document: Readonly<TextDocument>, initialCursorPosition: number, needle: string) {
     this.document = document
-    this.scanOrder = this.generateScanOrder(document, startLine)
+    this.scannerState = {
+      scanDirection: ScanDirection.Down,
+      initialCursorPosition,
+      upperCursorPosition: initialCursorPosition,
+      lowerCursorPosition: initialCursorPosition,
+      upperBoundary: 0,
+      lowerBoundary: document.lineCount - 1,
+      upperBoundaryReached: false,
+      lowerBoundaryReached: false,
+    }
+    this.iterationOrder = this.generateIterationOrder()
     this.documentIterator = this.createDocumentIterator(needle)
   }
 
-  private generateScanOrder = (document: Readonly<TextDocument>, startLine: number): number[] => {
-    const scanOrder: number[] = []
-    const documentLineCount = document.lineCount
+  private generateIterationOrder = (): number[] => {
+    const iterationOrder: number[] = [this.scannerState.initialCursorPosition]
+    const documentLineCount = this.document.lineCount
 
     if (documentLineCount === 1) {
-      scanOrder.push(startLine)
-      return scanOrder
-    }
-      
-    if (documentLineCount === 2) {
-      scanOrder.push(
-        startLine,
-        (startLine + 1) % 2
-      )
-
-      return scanOrder
+      return iterationOrder
     }
 
-    if (documentLineCount === 3) {
-      // This ordering ensures that the line below the start line, including
-      // wrap-around, is always scanned immediately after the start line.
-      scanOrder.push(
-        startLine,
-        (startLine - 2) % 3,
-        (startLine + 2) % 3
-      )
+    this.checkScannerBoundaries()
 
-      return scanOrder
-    }
-
-    const upperBoundary = 0
-    const lowerBoundary = documentLineCount - 1
-    const shiftAwayFromBoundary = this.isStartLineInTopHalf(startLine, documentLineCount) ? 1 : -1
-    let currentLine = startLine
-    let hasHitBoundary = false
+    do {
+      const nextCursorPosition = this.shiftCursorAndReverseScanDirection()
+      iterationOrder.push(nextCursorPosition)
+      this.checkScannerBoundaries()
+    } while (
+      iterationOrder.length < documentLineCount
+      && iterationOrder.length < DocumentScanner.ITERATION_LIMIT
+    )
     
-    if (startLine === upperBoundary || startLine === lowerBoundary) {
-      scanOrder.push(
-        currentLine,
-        currentLine + (2 * shiftAwayFromBoundary),
-        currentLine + (3 * shiftAwayFromBoundary)
-      )
-
-      currentLine += (3 * shiftAwayFromBoundary)
-      hasHitBoundary = true
-    } else {
-      scanOrder.push(
-        currentLine,
-        currentLine + 1,
-        currentLine - 1
-      )
-    }
-
-    if (startLine + 2 > lowerBoundary) {
-      currentLine -= 2
-      hasHitBoundary = true
-    } else {
-      currentLine += 2
-    }
-
-    while (
-      scanOrder.length < documentLineCount
-      && scanOrder.length <= DocumentScanner.MAX_LINES_TO_SCAN
-    ) {
-      scanOrder.push(currentLine)
-
-      if (hasHitBoundary) {
-        currentLine += shiftAwayFromBoundary
-        continue
-      }
-
-      const pivotedLine = this.pivotLine(startLine, currentLine)
-
-      if (pivotedLine < upperBoundary || pivotedLine > lowerBoundary) {
-        currentLine += shiftAwayFromBoundary
-        hasHitBoundary = true
-
-        continue
-      }
-
-      if (currentLine === upperBoundary || currentLine === lowerBoundary) {
-        hasHitBoundary = true
-      }
-
-      currentLine = pivotedLine
-    }
-
-    return scanOrder
+    return iterationOrder
   }
 
-  private isStartLineInTopHalf = (startLine: number, documentLineCount: number): boolean => {
-    return startLine <= Math.floor(documentLineCount) / 2
+  private checkScannerBoundaries = () => {
+    if (this.scannerState.upperCursorPosition <= this.scannerState.upperBoundary) {
+      this.scannerState.upperBoundaryReached = true
+      this.scannerState.scanDirection = ScanDirection.Down
+    }
+
+    if (this.scannerState.lowerCursorPosition >= this.scannerState.lowerBoundary) {
+      this.scannerState.lowerBoundaryReached = true
+      this.scannerState.scanDirection = ScanDirection.Up
+    }
   }
 
-  private pivotLine = (startLine: number, currentLine: number): number => {
-    const distanceBetweenStartAndCurrentLine = Math.abs(currentLine - startLine)
-    const pivotDistance = 2 * distanceBetweenStartAndCurrentLine
-    const isCurrentLineInTopHalf = currentLine < startLine
+  private shiftCursorAndReverseScanDirection = (): number => {
+    if (this.scannerState.scanDirection === ScanDirection.Down) {
+      this.scannerState.lowerCursorPosition += 1
+      this.scannerState.scanDirection = ScanDirection.Up
 
-    if (isCurrentLineInTopHalf) {
-      return currentLine + pivotDistance + 1
+      return this.scannerState.lowerCursorPosition
     } else {
-      return currentLine - pivotDistance
+      this.scannerState.upperCursorPosition -= 1
+      this.scannerState.scanDirection = ScanDirection.Down
+
+      return this.scannerState.upperCursorPosition
     }
   }
 
   private* createDocumentIterator(needle: string): Iterator<any> {
-    for (const currentLine of this.scanOrder) {
+    for (const currentLine of this.iterationOrder) {
       const haystack = this.getLineText(currentLine).toLowerCase()
 
       for (let needleSearchResumePosition = 0;;) {
